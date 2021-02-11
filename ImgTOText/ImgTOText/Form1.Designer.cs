7a1e9bbcd537eb5b5d208bcf8b00959d44175989
Aktam <abdunazarov9798@gmail.com>
2021-02-11 23:41:19 +0300

    create ImageToText

---
+++ ImgTOText/ImgTOText/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace ImgTOText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(249, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ochish";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 339);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(346, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(328, 352);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(183, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Convert Text";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Coral;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(579, 414);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 24);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Export PDF";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(429, 388);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(245, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter File name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(686, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Image To Text";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImgTOText/ImgTOText/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronOcr;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+
+
+namespace ImgTOText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        string fileName;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = dialog.FileName;
+                textBox1.Text = fileName;
+                 
+            }
+            try
+            {
+              pictureBox1.Image = new Bitmap(fileName);
+            }
+            catch
+            {
+                MessageBox.Show("Fayl noto'ri tanlangan rasm tipidagi fayl tanlang");
+            }
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var Ocr = new IronTesseract();
+            using(var input=new OcrInput(fileName))
+            {
+                input.Deskew();
+                var result = Ocr.Read(input);
+                richTextBox1.Text = result.Text;
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text!="")
+            {
+                
+                  
+                  
+                    PdfDocument doc = new PdfDocument();
+                    PdfSection section = doc.Sections.Add();
+                    PdfPageBase page = section.Pages.Add();
+                    PdfFont font = new PdfFont(PdfFontFamily.Helvetica, 11);
+                    PdfStringFormat format = new PdfStringFormat();
+                    format.LineSpacing = 20f;
+                    PdfBrush brush = PdfBrushes.Black;
+                    PdfTextWidget textWidget = new PdfTextWidget(richTextBox1.Text, font, brush);
+                    float y = 0;
+                    PdfTextLayout textLayout = new PdfTextLayout();
+                    textLayout.Break = PdfLayoutBreakType.FitPage;
+                    textLayout.Layout = PdfLayoutType.Paginate;
+                    RectangleF bounds = new RectangleF(new PointF(0, y), page.Canvas.ClientSize);
+                    textWidget.StringFormat = format;
+                    textWidget.Draw(page, bounds, textLayout);
+                    doc.SaveToFile("E:\\" + textBox2.Text + ".pdf", FileFormat.PDF);
+                    MessageBox.Show("Amal bajarildi","Bajarildi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    textBox2.Text = null;
+                    richTextBox1.Text = null;
+                    pictureBox1.Image = null;
+                    textBox1.Text = null;
+                
+            }
+           
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
